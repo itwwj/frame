a8081b70db3878cc7f988ee8c708aa0b443b1a1a
王文杰 <275236367@qq.com>
2020-12-12 18:42:46 +0800

    优化docker脚本

---
+++ data-mongo/src/main/java/com/github/mongo/config/ListenerMongo.java
@@ -0,0 +1,10 @@
+package com.github.mongo.config;
+
+import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
+
+/**
+ * 生命周期事件
+ * @author jie
+ */
+public class ListenerMongo extends AbstractMongoEventListener {
+}
